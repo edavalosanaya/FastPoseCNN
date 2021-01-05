stable, fixed memory leak, nan dominated epoch logs, and partially gradient wipeout due to high quaternion loss

source_code/FastPoseCNN/callbacks.py
@@ -99,7 +99,10 @@ class MyCallback(pl.callbacks.Callback):
 
                 # Delete previous checkpoint (if it exist)
                 if monitor_data['saved_checkpoint_fp']:
-                    os.remove(monitor_data['saved_checkpoint_fp'])
+                    try:
+                        os.remove(monitor_data['saved_checkpoint_fp'])
+                    except FileNotFoundError:
+                        pass
 
                 # Generating new checkpoint filepath
                 safe_monitor_name = monitor_name.replace("/", "_")
@@ -161,10 +164,14 @@ class MyCallback(pl.callbacks.Callback):
             if tb_log_name in sum(self.tracked_data.values(), []):
 
                 # Move all items inside the logger into cuda:0
-                cuda_0_log = [x.to('cuda:0') for x in log[log_name]]
+                cuda_0_log = [x.to('cuda:0').float() for x in log[log_name] if torch.isnan(x) != True]
 
-                # Obtain the average first
-                average = torch.mean(torch.stack(cuda_0_log))
+                # If cuda_0_log is not empty, then determine the average
+                if cuda_0_log:
+                    average = torch.mean(torch.stack(cuda_0_log))
+                
+                else: # else just use nan as the average
+                    average = torch.tensor(float('nan')).to('cuda:0').float()
 
                 # Log the average
                 trainer.logger.log_metrics(

source_code/FastPoseCNN/lib/loss.py
@@ -100,7 +100,7 @@ class MaskedMSELoss(_Loss):
 
         # Return 1 if no matching between masks
         if torch.sum(mask_union) == 0:
-            return torch.tensor(1, device=cat_mask.device).float()
+            return torch.tensor(float('nan'), device=cat_mask.device).float()
 
         # Access the predictions to calculate the loss (NxAxHxW) A = [3,4]
         y_pred = pred[self.key]
@@ -278,7 +278,7 @@ class AggregatedQLoss(_Loss):
         if losses:
             stacked_losses = torch.cat(losses)
         else:
-            return torch.tensor(float('nan')).cuda()
+            return torch.tensor(float('nan')).cuda().float()
 
         # Return the some of all the losses
         return torch.mean(stacked_losses)

source_code/FastPoseCNN/train.py
@@ -74,11 +74,11 @@ class DEFAULT_POSE_HPARAM(argparse.Namespace):
     SELECTED_CLASSES = tools.pj.constants.NUM_CLASSES[DATASET_NAME]
 
     # Run Specifications
-    BATCH_SIZE = 2
+    BATCH_SIZE = 8
     NUM_WORKERS = 18 # 36 total CPUs
-    NUM_GPUS = 2
-    TRAIN_SIZE=100#5000
-    VALID_SIZE=10#200
+    NUM_GPUS = 1
+    TRAIN_SIZE=5000
+    VALID_SIZE=200
 
     # Training Specifications
     FREEZE_ENCODER = False
@@ -209,13 +209,16 @@ class PoseRegresssionTask(pl.LightningModule):
                 gt_pred_matches
             )
 
+            # Looking out for memory leakage here
+            #lib.gtf.memory_leak_check()
+
             # Logging the batch loss to Tensorboard
             for loss_name, loss_value in losses.items():
-                self.logger.log_metrics(mode, {f'{task_name}/{loss_name}/batch':loss_value}, batch_idx)
+                self.logger.log_metrics(mode, {f'{task_name}/{loss_name}/batch':loss_value.detach().clone()}, batch_idx)
 
             # Logging the metric loss to Tensorboard
             for metric_name, metric_value in metrics.items():
-                self.logger.log_metrics(mode, {f'{task_name}/{metric_name}/batch':metric_value}, batch_idx) 
+                self.logger.log_metrics(mode, {f'{task_name}/{metric_name}/batch':metric_value.detach().clone()}, batch_idx) 
 
             # Storing the losses and metrics for each task
             multi_task_losses[task_name] = losses
@@ -283,16 +286,15 @@ class PoseRegresssionTask(pl.LightningModule):
         else: # otherwise, just pass losses as nan
 
             # Place nan in losses (for PyTorch progress bar logger)
-            losses['loss'] = torch.tensor(float('nan')).to(self.device)
+            losses['loss'] = torch.tensor(float('nan')).to(self.device).float()
 
             # Notate no task-specific total loss (for customized Tensorboard logger)
-            losses['task_total_loss'] = torch.tensor(float('nan')).to(self.device)
+            losses['task_total_loss'] = torch.tensor(float('nan')).to(self.device).float()
 
         # Looking for the invalid tensor in the cpu
         for k,v in losses.items():
-            if v.device == 'cpu':
-                print(k)
-                raise RuntimeError('Invalid tensor')
+            if v.device == torch.device('cpu'):
+                raise RuntimeError(f'Invalid cpu tensor: {k}')
 
         return losses, metrics
 
@@ -451,8 +453,8 @@ if __name__ == '__main__':
             'loss_focal': {'D': 'pixel-wise', 'F': lib.loss.Focal(), 'weight': 1.0}
         },
         'quaternion': {
-            #'loss_qloss': {'D': 'matched', 'F': lib.loss.AggregatedQLoss(key='quaternion'), 'weight': 1.0},
-            'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='quaternion'), 'weight': 1.0},
+            'loss_qloss': {'D': 'matched', 'F': lib.loss.AggregatedQLoss(key='quaternion'), 'weight': 1.0},
+            #'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='quaternion'), 'weight': 1.0},
             #'loss_pw_qloss': {'D': 'pixel-wise', 'F': lib.loss.PixelWiseQLoss(key='quaternion'), 'weight': 1.0}
         },
         'xy': {