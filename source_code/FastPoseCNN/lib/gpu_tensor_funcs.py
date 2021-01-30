Fixed gradient Nan explosion and added new loss functions

source_code/FastPoseCNN/lib/gpu_tensor_funcs.py
@@ -358,9 +358,9 @@ def batchwise_find_matches(preds, gts):
     pred_gt_matches = []
     keys_to_stack = [
         'instance_masks', # Class
-        'quaternion', # Rotation
+        'quaternion', 'R', # Rotation
         'scales', # Size
-        'xy', 'z', # Translation
+        'xy', 'z', 'T', # Translation
         'RT', # Transformation
         'xy_mask', 'hypothesis', 'pruned_hypothesis' # Hough Voting
     ]
@@ -410,16 +410,18 @@ def batchwise_find_matches(preds, gts):
         class_data['sample_ids'] = gts[class_id]['sample_ids'][max_gt_id]
 
         # Select the match data and combined them together!
-        for data_key in keys_to_stack:
-            stacked_data = torch.stack(
-                (
-                    gts[class_id][data_key][max_gt_id],
-                    preds[class_id][data_key][max_pred_id]
+        for data_key in gts[class_id].keys():
+            if data_key in keys_to_stack:
+                # Stack data
+                stacked_data = torch.stack(
+                    (
+                        gts[class_id][data_key][max_gt_id],
+                        preds[class_id][data_key][max_pred_id]
+                    )
                 )
-            )
 
-            # Store stacked data
-            class_data[data_key] = stacked_data
+                # Store stacked data
+                class_data[data_key] = stacked_data
 
         # Store the class-specific data into the multi-class data container
         pred_gt_matches.append(class_data)
@@ -600,7 +602,7 @@ def batchwise_get_RT(q, xys, exp_zs, inv_intrinsics):
     # Inversing it to get the actual RT
     RT = torch.inverse(inv_RT)
 
-    return RT
+    return R, T.t(), RT
 
 def samplewise_get_RT(agg_data, inv_intrinsics):
 
@@ -608,7 +610,7 @@ def samplewise_get_RT(agg_data, inv_intrinsics):
 
         # Once all the raw data has been aggregated, we need to calculate the 
         # rotation matrix of each instance.
-        RT_data = batchwise_get_RT(
+        R_data, T_data, RT_data = batchwise_get_RT(
             agg_data[class_id]['quaternion'],
             agg_data[class_id]['xy'],
             agg_data[class_id]['z'],
@@ -616,6 +618,8 @@ def samplewise_get_RT(agg_data, inv_intrinsics):
         )
 
         # Storing generated RT
+        agg_data[class_id]['R'] = R_data
+        agg_data[class_id]['T'] = T_data
         agg_data[class_id]['RT'] = RT_data
 
     return agg_data

source_code/FastPoseCNN/lib/hough_voting.py
@@ -185,6 +185,10 @@ class HoughVotingLayer(nn.Module):
             chunked_valid_Y = torch.chunk(valid_Y, len(valid_samples))
             valid_samples = {k:v for k,v in zip(valid_samples, chunked_valid_Y)}
 
+        else:
+            # Simply to be consistant in data type for latter part of the algorithm.
+            valid_samples = {}
+
         # Splitting the total_Y based on the number of instances
         total_Y = torch.zeros(
             (uv_img.shape[0], self.HPARAM.HV_NUM_OF_HYPOTHESES, 2),
@@ -243,7 +247,7 @@ class HoughVotingLayer(nn.Module):
             weights = factor * weights
 
             # Normalizing weight
-            weights = weights / torch.sum(weights)
+            weights = weights / max(torch.sum(weights), 1)
 
             # Store weights
             all_weights.append(weights)

source_code/FastPoseCNN/lib/loss.py
@@ -82,7 +82,7 @@ class Focal(_Loss):
         return loss(y_pred, y_true)
 
 #-------------------------------------------------------------------------------
-# Pose Losses
+# General pixel-wise loss function
 
 class MaskedMSELoss(_Loss):
 
@@ -133,6 +133,9 @@ class MaskedMSELoss(_Loss):
 
         return masked_loss
 
+#-------------------------------------------------------------------------------
+# Quaternion specific loss function
+
 class PixelWiseQLoss(_Loss):
 
     def __init__(self, key, eps=0.001):
@@ -156,17 +159,8 @@ class PixelWiseQLoss(_Loss):
         gt_q = gt[self.key]
         pred_q = pred[self.key]
 
-        # Obtaining the magnitude of the quaternion to later normalize
-        norm_q = pred_q.norm(dim=1)
-        
-        # Avoid dividing by zero
-        norm_q[norm_q == 0] = 1
-
-        # Expanding the shape of norm_q to match with m_pred_q
-        norm_q = torch.unsqueeze(norm_q, dim=1)
-
         # Normalizing the masked predicted quaternions
-        q = torch.div(pred_q, norm_q)
+        q = torch.div(pred_q, pred_q)
 
         # Apply the QLoss Function
         # log(\epsilon + 1 - |gt_q dot pred_q|)
@@ -181,23 +175,23 @@ class PixelWiseQLoss(_Loss):
         """
         dot_product = gt_q[:,0] * q[:,0] + gt_q[:,1] * q[:,1] + gt_q[:,2] * q[:,2] + gt_q[:,3] * q[:,3]
         mag_dot_product = torch.pow(dot_product, 2)
-        difference = self.eps + 1 - mag_dot_product
+        error = self.eps + 1 - mag_dot_product
+        loss = torch.log(error + self.eps) - torch.log(torch.tensor(self.eps, device=error.device))
 
-        # Mask the difference based on the union mask
-        difference[mask_union == False] = 0
-        masked_loss = torch.sum(difference) / torch.sum(mask_union)
+        # Mask the loss based on the union mask
+        loss[mask_union == False] = 0
+        masked_loss = torch.sum(loss) / torch.sum(mask_union)
 
         return masked_loss
 
-class AggregatedQLoss(_Loss):
-    """AggregatedQLoss
+class AggregatedLoss(_Loss):
+    """AggregatedLoss
+    Generic loss function for aggregated data
 
-    References:
+    Quaternion References:
     https://math.stackexchange.com/questions/90081/quaternion-distance
     http://kieranwynn.github.io/pyquaternion/#normalisation
     https://github.com/KieranWynn/pyquaternion/blob/99025c17bab1c55265d61add13375433b35251af/pyquaternion/quaternion.py#L800
-    
-
 
     Args:
         _Loss ([type]): [description]
@@ -206,85 +200,191 @@ class AggregatedQLoss(_Loss):
         [type]: [description]
     """
 
-    def __init__(self, key, eps=0.001):
-        super(AggregatedQLoss, self).__init__()
+    def __init__(self, key, eps=0.1):
+        super(AggregatedLoss, self).__init__()
         self.key = key
         self.eps = eps
 
-    def forward(self, matches) -> Tensor:
+    def forward(self, gt_pred_matches) -> Tensor:
         """AggregatedQLoss Foward
 
         Args:
             matches [list]: 
                 match ([dict]):
                     class_id: torch.Tensor
-                    quaternion: torch.Tensor
 
         Returns:
             Tensor: [description]
         """
 
-        # Create matches that are only true
-        true_gt_pred_matches = []
-
-        # Remove false matches (meaning 'iou_2d_mask')
-        for match in matches:
-
-            # Keeping the match if the iou_2d_mask > 0
-            if match['iou_2d_mask'] > 0:
-                true_gt_pred_matches.append(match)
-
-        # If there is no true matches, simply end update function
-        if true_gt_pred_matches == []:
-            return torch.tensor(float('nan')).float().cuda()
-
-        # Stack the matches based on class for all quaternion
-        stacked_class_quaternion = gtf.stack_class_matches(true_gt_pred_matches, 'quaternion')
-
         # Container for per class collective loss
-        per_class_loss = {}
+        per_class_loss = []
 
         # Apply the QLoss Function 
         # log(\epsilon + 1 - |gt_q dot pred_q|)
-        for class_number in stacked_class_quaternion.keys():
+        for class_id in range(len(gt_pred_matches)):
 
-            # Selecting the ground truth quaternion
-            gt_q = stacked_class_quaternion[class_number][:,0,:]
+            # Catching no-instance scenario
+            if self.key not in gt_pred_matches[class_id].keys():
+                continue
 
-            # Selecting the predicted quaternion
-            pred_q = stacked_class_quaternion[class_number][:,1,:]
+            # Selecting the ground truth data
+            gt = gt_pred_matches[class_id][self.key][0]
 
-            # Normalize the predicted quaternion
-            norm_q = torch.div(pred_q.T, pred_q.norm(dim=1).T).T
+            # Selecting the predicted data
+            pred = gt_pred_matches[class_id][self.key][1]
 
             # Calculating the loss
-            """
-            dot_product = torch.diag(torch.mm(gt_q, norm_q.T))
-            mag_dot_product = torch.abs(dot_product)
-            difference = self.eps + 1 - mag_dot_product
-            log_difference = -torch.log(difference)
-            per_class_loss[class_number] = log_difference
-            """
-            dot_product = torch.diag(torch.mm(gt_q, norm_q.T))
-            mag_dot_product = torch.pow(dot_product, 2)
-            difference = self.eps + 1 - mag_dot_product
-            per_class_loss[class_number] = difference
+            if self.key == 'quaternion':
+                dot_product = torch.diag(torch.mm(gt, pred.T))
+                mag_dot_product = torch.pow(dot_product, 2)
+                error = 1 - mag_dot_product
+                loss = torch.log(error + self.eps) - torch.log(torch.tensor(self.eps, device=error.device))
+            
+            elif self.key == 'xy':
+                loss = (gt-pred).norm(dim=1) / 10
+            
+            elif self.key == 'z':
+                loss = (torch.log(gt)-torch.log(pred)).norm(dim=1)
+            
+            elif self.key in ['scales', 'T']:
+                loss = (gt-pred).norm(dim=1)
+            
+            elif self.key == 'R':
+                loss = torch.acos((torch.einsum('bii->b', torch.bmm(gt, pred)) - 1) / 2)
+            
+            elif self.key == 'RT':
+                loss = (torch.inverse(gt) * pred).norm(dim=1)
 
-        # Place all the class losses into a single list
-        losses = [v for v in per_class_loss.values()]
+            else:
+                raise NotImplementedError("Invalid entered key.")
 
-        # Stack the losses to later sum the loss
+            # Storing the loss per class
+            per_class_loss.append(loss)
+
+        # Concatenate the losses to later sum the loss
         # If not empty
-        if losses:
-            stacked_losses = torch.cat(losses)
+        if per_class_loss:
+            cat_losses = torch.cat(per_class_loss)
         else:
-            return torch.tensor(float('nan')).cuda().float()
+            try:
+                return torch.tensor(float('nan'), device=gt_pred_matches[0]['instance_masks'].device).float()   
+            except:
+                return torch.tensor(float('nan')).cuda().float()   
+
+        # Remove any nans in the data
+        cat_losses = cat_losses[torch.isnan(cat_losses) == False]
 
         # Return the some of all the losses
-        return torch.mean(stacked_losses)
-        
+        return torch.mean(cat_losses)
 
-            
+#-------------------------------------------------------------------------------
+# Pose loss functions
+
+class Iou3dLoss(_Loss):  
+
+    def __init__(self, eps=0.1):
+        super(Iou3dLoss, self).__init__()
+        self.eps = eps
+
+    def forward(self, gt_pred_matches) -> Tensor:
+
+        # Container for per class collective loss
+        per_class_loss = []
+
+        for class_id in range(len(gt_pred_matches)):
+
+            # Catching no-instance scenario
+            if 'RT' not in gt_pred_matches[class_id].keys():
+                continue
+
+            # Grabbing the gt and pred (RT and scales)
+            gt_RTs = gt_pred_matches[class_id]['RT'][0]
+            gt_scales = gt_pred_matches[class_id]['scales'][0]
+            pred_RTs = gt_pred_matches[class_id]['RT'][1]
+            pred_scales = gt_pred_matches[class_id]['scales'][1]
+
+            # Calculating the iou 3d for between the ground truth and predicted 
+            ious_3d = gtf.get_3d_ious(gt_RTs, pred_RTs, gt_scales, pred_scales)
+
+            # Calculating the error
+            error = 1 - ious_3d
+
+            # Calculating the loss
+            loss = error
+            #loss = torch.log(error + self.eps) - torch.log(torch.tensor(self.eps, device=error.device))
+
+            # Storing the loss per class
+            print(loss)
+            per_class_loss.append(loss)
+
+        # Concatenate the losses to later sum the loss
+        # If not empty
+        if per_class_loss:
+            cat_losses = torch.cat(per_class_loss)
+        else:
+            try:
+                return torch.tensor(float('nan'), device=gt_pred_matches[0]['instance_masks'].device).float()   
+            except:
+                return torch.tensor(float('nan')).cuda().float()   
+
+        # Remove any nans in the data
+        cat_losses = cat_losses[torch.isnan(cat_losses) == False]
+
+        # Return the some of all the losses
+        return torch.mean(cat_losses)
+
+class OffsetLoss(_Loss):  
+
+    def __init__(self, eps=0.1):
+        super(OffsetLoss, self).__init__()
+        self.eps = eps
+
+    def forward(self, gt_pred_matches) -> Tensor:
+
+        # Container for per class collective loss
+        per_class_loss = []
+
+        for class_id in range(len(gt_pred_matches)):
+
+            # Catching no-instance scenario
+            if 'scales' not in gt_pred_matches[class_id].keys():
+                continue
+
+            # Grabbing the gt and pred (RT and scales)
+            gt_RTs = gt_pred_matches[class_id]['RT'][0]
+            pred_RTs = gt_pred_matches[class_id]['RT'][1]
+
+            # Determing the offset errors
+            offset_errors = gtf.from_RTs_get_T_offset_errors(
+                gt_RTs,
+                pred_RTs
+            )
+
+            # Calculating the loss
+            #loss = torch.log(offset_errors + self.eps) - torch.log(torch.tensor(self.eps, device=offset_errors.device))
+            loss = offset_errors / 100
+
+            # Storing the loss per class
+            per_class_loss.append(loss)
+
+        # Concatenate the losses to later sum the loss
+        # If not empty
+        if per_class_loss:
+            cat_losses = torch.cat(per_class_loss)
+        else:
+            try:
+                return torch.tensor(float('nan'), device=gt_pred_matches[0]['instance_masks'].device).float()   
+            except:
+                return torch.tensor(float('nan')).cuda().float()   
+
+        # Remove any nans in the data
+        cat_losses = cat_losses[torch.isnan(cat_losses) == False]
+
+        # Return the some of all the losses
+        return torch.mean(cat_losses)
+
+
 
 
 

source_code/FastPoseCNN/lib/metrics.py
@@ -139,7 +139,7 @@ class Iou3dAP(pl.metrics.Metric):
             ious_3d = gtf.get_3d_ious(gt_RTs, pred_RTs, gt_scales, pred_scales)
 
             # Compare against threshold
-            thresh_iou_3d = (ious_3d < self.threshold)
+            thresh_iou_3d = (ious_3d > self.threshold)
 
             # Update complete and total
             self.correct = self.correct + torch.sum(thresh_iou_3d.int())
@@ -148,13 +148,13 @@ class Iou3dAP(pl.metrics.Metric):
     def compute(self):
         return (self.correct.float() / self.total.float()) * 100
 
-class Iou3dError(pl.metrics.Metric):
+class Iou3dAccuracy(pl.metrics.Metric):
 
     def __init__(self):
-        super().__init__(f'3D_iou_error')
+        super().__init__(f'3D_iou_accuracy')
 
         # Adding state data
-        self.add_state('error', default=torch.tensor(0), dist_reduce_fx='mean')
+        self.add_state('accuracy', default=torch.tensor(0), dist_reduce_fx='mean')
 
     def update(self, gt_pred_matches):
         """
@@ -183,25 +183,25 @@ class Iou3dError(pl.metrics.Metric):
             pred_scales = gt_pred_matches[class_id]['scales'][1]
 
             # Calculating the iou 3d for between the ground truth and predicted 
-            ious_3d = gtf.get_3d_ious(gt_RTs, pred_RTs, gt_scales, pred_scales)
+            ious_3d = gtf.get_3d_ious(gt_RTs, pred_RTs, gt_scales, pred_scales) * 100
 
             # Storing all the ious to later concat and take average
             all_ious_3d.append(ious_3d)
 
         # This rounds accuracy
-        this_round_error = torch.mean(torch.cat(all_ious_3d))
+        this_round_accuracy = torch.mean(torch.cat(all_ious_3d))
 
         # Update the mean accuracy
-        self.error = (self.error + this_round_error) / 2
+        self.accuracy = (self.accuracy + this_round_accuracy) / 2
 
     def compute(self):
-        return self.error
+        return self.accuracy
 
 class OffsetAP(pl.metrics.Metric):
 
-    def __init__(self, threshold_cm):
-        super().__init__(f'offset_error_mAP_{threshold_cm}cm')
-        self.threshold = threshold_cm / 100 # converting from cm to m
+    def __init__(self, threshold):
+        super().__init__(f'offset_error_mAP_{threshold}cm')
+        self.threshold = threshold
 
         # Adding state data
         self.add_state('correct', default=torch.tensor(0), dist_reduce_fx='sum')
@@ -282,9 +282,6 @@ class OffsetError(pl.metrics.Metric):
                 gt_RTs,
                 pred_RTs
             )
-
-            # Convert the offset (m) to (cm)
-            offset_errors *= 100
 
             # This rounds accuracy
             this_round_error = torch.mean(offset_errors)

source_code/FastPoseCNN/train.py
@@ -3,6 +3,7 @@ import warnings
 import datetime
 import argparse
 import pathlib
+from pprint import pprint
 
 import pdb
 
@@ -17,6 +18,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 #os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 import torch
+import torch.optim
 import torch.nn.functional as F
 
 import pytorch_lightning as pl
@@ -124,8 +126,7 @@ class PoseRegresssionTask(pl.LightningModule):
         # Calculate the loss
         multi_task_losses, multi_task_metrics = self.shared_step('valid', batch, batch_idx)
         
-        # Log the batch loss inside the pl.TrainResult to visualize in the
-        # progress bar
+        # Log the batch loss inside the pl.TrainResult to visualize in the progress bar
         result = pl.core.step_result.EvalResult(checkpoint_on=multi_task_losses['pose']['total_loss'])
 
         # Logging the val loss for each task
@@ -179,7 +180,10 @@ class PoseRegresssionTask(pl.LightningModule):
             )
 
             # Storing the task losses
-            multi_task_losses[task_name] = losses
+            if task_name not in multi_task_losses.keys():
+                multi_task_losses[task_name] = losses
+            else:
+                multi_task_losses[task_name].update(losses)
 
             # Summing all task total losses (if it is not nan)
             if torch.isnan(losses['task_total_loss']) != True:
@@ -235,10 +239,10 @@ class PoseRegresssionTask(pl.LightningModule):
         if true_losses:
 
             # Calculate total loss
-            total_loss = torch.sum(torch.stack(true_losses))
+            #total_loss = torch.sum(torch.stack(true_losses))
 
             # Calculate the loss multiplied by its corresponded weight
-            weighted_losses = [losses[key] * self.criterion[task_name][key]['weight'] for key in losses.keys()]
+            weighted_losses = [losses[key] * self.criterion[task_name][key]['weight'] for key in losses.keys() if torch.isnan(losses[key]) == False]
             
             # Now calculate the weighted sum
             weighted_sum = torch.sum(torch.stack(weighted_losses))
@@ -247,7 +251,7 @@ class PoseRegresssionTask(pl.LightningModule):
             losses['loss'] = weighted_sum
 
             # Saving the total loss (for customized Tensorboard logger)
-            losses['task_total_loss'] = total_loss
+            losses['task_total_loss'] = weighted_sum #total_loss
 
         else: # otherwise, just pass losses as nan
 
@@ -258,9 +262,11 @@ class PoseRegresssionTask(pl.LightningModule):
             losses['task_total_loss'] = torch.tensor(float('nan')).to(self.device).float()
 
         # Looking for the invalid tensor in the cpu
+        """
         for k,v in losses.items():
             if v.device != self.device:
                 raise RuntimeError(f'Invalid tensor for not being in the same device as the pl module: {k}')
+        """
 
         return losses
 
@@ -293,7 +299,10 @@ class PoseRegresssionTask(pl.LightningModule):
     def configure_optimizers(self):
 
         # Catalyst has new SOTA optimizers out of box
-        base_optimizer = catalyst.contrib.nn.RAdam(self.model.parameters(), lr=HPARAM.LEARNING_RATE, weight_decay=0.0003)
+        base_optimizer = catalyst.contrib.nn.RAdam(self.model.parameters(), lr=HPARAM.LEARNING_RATE, weight_decay=HPARAM.WEIGHT_DECAY)
+        #base_optimizer = torch.optim.SGD(self.model.parameters(), lr=HPARAM.LEARNING_RATE, weight_decay=HPARAM.WEIGHT_DECAY)
+        #base_optimizer = torch.optim.Adagrad(self.model.parameters(), lr=HPARAM.LEARNING_RATE, weight_decay=HPARAM.WEIGHT_DECAY)
+        #base_optimizer = torch.optim.Adam(self.model.parameters(), lr=HPARAM.LEARNING_RATE, weight_decay=HPARAM.WEIGHT_DECAY)
         optimizer = catalyst.contrib.nn.Lookahead(base_optimizer)
 
         # Solution from here:
@@ -448,8 +457,7 @@ if __name__ == '__main__':
             'loss_focal': {'D': 'pixel-wise', 'F': lib.loss.Focal(), 'weight': 1.0}
         },
         'quaternion': {
-            #'loss_qloss': {'D': 'matched', 'F': lib.loss.AggregatedQLoss(key='quaternion'), 'weight': 1.0},
-            'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='quaternion'), 'weight': 1.0},
+            'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='quaternion'), 'weight': 0.2},
             #'loss_pw_qloss': {'D': 'pixel-wise', 'F': lib.loss.PixelWiseQLoss(key='quaternion'), 'weight': 1.0}
         },
         'xy': {
@@ -459,7 +467,18 @@ if __name__ == '__main__':
             'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='z'), 'weight': 1.0}
         },
         'scales': {
-            'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='scales'), 'weight': 1.0}
+            'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='scales'), 'weight': 1.0},
+        },
+        'aggregated': {
+            #'loss_quat': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='quaternion'), 'weight': 1.0},
+            #'loss_xy': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='xy'), 'weight': 1.0},
+            #'loss_z': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='z'), 'weight': 1.0},
+            #'loss_scales': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='scales'), 'weight': 1.0},
+            #'loss_R': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='R'), 'weight': 1.0},
+            #'loss_T': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='T'), 'weight': 1.0},
+            #'loss_RT': {'D': 'matched', 'F': lib.loss.AggregatedLoss(key='RT'), 'weight': 1.0}
+            'loss_iou3d': {'D': 'matched', 'F': lib.loss.Iou3dLoss(), 'weight': 1.0},
+            #'loss_offset': {'D': 'matched', 'F': lib.loss.OffsetLoss(), 'weight': 1.0}
         }
     }
 
@@ -485,8 +504,8 @@ if __name__ == '__main__':
         'pose': {
             'degree_error': {'D': 'matched', 'F': lib.metrics.DegreeError()},
             'degree_error_AP_5': {'D': 'matched', 'F': lib.metrics.DegreeErrorMeanAP(5)},
-            'iou_3d_mAP_5': {'D': 'matched', 'F': lib.metrics.Iou3dAP(5)},
-            'iou_3d_error': {'D': 'matched', 'F': lib.metrics.Iou3dError()},
+            'iou_3d_mAP_0.25': {'D': 'matched', 'F': lib.metrics.Iou3dAP(0.25)},
+            'iou_3d_accuracy': {'D': 'matched', 'F': lib.metrics.Iou3dAccuracy()},
             'offset_error_AP_5cm': {'D': 'matched', 'F': lib.metrics.OffsetAP(5)},
             'offset_error': {'D': 'matched', 'F': lib.metrics.OffsetError()},
         }
@@ -618,7 +637,8 @@ if __name__ == '__main__':
         num_processes=HPARAM.NUM_WORKERS,
         distributed_backend=HPARAM.DISTRIBUTED_BACKEND, # required to work
         logger=tb_logger,
-        callbacks=[custom_callback, loss_checkpoint_callback]
+        callbacks=[custom_callback, loss_checkpoint_callback],
+        gradient_clip_val=0.5
     )
 
     # Train