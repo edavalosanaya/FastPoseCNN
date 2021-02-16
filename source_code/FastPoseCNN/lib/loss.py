Fixed visualization, alter quaternion rotation transformations to include all 360 rotations, increase gradient clipping, and filled nan and inf data with zeros in forward funciton of the pose regression task class.

source_code/FastPoseCNN/lib/loss.py
@@ -295,8 +295,8 @@ class QLoss(_Loss): # Quaternion
             return torch.tensor([float('nan')], device=gt.device)
 
         # ! For DEBUGGING/TESTING 
-        pred = torch.tensor([[0.7071, 0, 0, 0.7071]], device=pred.device)
-        gt = torch.tensor([[0,0,0,1]], device=gt.device)
+        #pred = torch.tensor([[0.7071, 0, 0, 0.7071]], device=pred.device)
+        #gt = torch.tensor([[0,0,0,1]], device=gt.device)
 
         # Expanding the pred to account for 0-360 degrees of rotation to account 
         # for z-axis symmetric and expanding the gt to match its size for later 
@@ -422,7 +422,13 @@ class XYLoss(_Loss): # 2D Center
             pred = gt_pred_matches[self.key][1]
 
             # Calculating the loss
-            loss = (gt-pred).norm(dim=1) / 10
+            abs_diff = torch.abs(gt-pred)
+
+            # Remove any nans in the data
+            abs_diff = abs_diff[torch.isnan(abs_diff) == False]
+
+            # Calculating the loss
+            loss = torch.mean(abs_diff) / 50 # Reducing the loss by a factor 
 
         else:
             try:
@@ -430,11 +436,8 @@ class XYLoss(_Loss): # 2D Center
             except:
                 return torch.tensor(float('nan')).cuda().float()   
 
-        # Remove any nans in the data
-        clean_loss = loss[torch.isnan(loss) == False]
-
         # Return the some of all the losses
-        return torch.mean(clean_loss)
+        return loss
 
 class ZLoss(_Loss): # Z - depth
     
@@ -458,8 +461,14 @@ class ZLoss(_Loss): # Z - depth
             # Selecting the predicted data
             pred = gt_pred_matches[self.key][1]
 
+            # Calculating the loss (logging the data first)
+            abs_diff = torch.abs(torch.log(gt)-torch.log(pred))
+
+            # Remove any nans in the data
+            abs_diff = abs_diff[torch.isnan(abs_diff) == False]
+
             # Calculating the loss
-            loss = (torch.log(gt)-torch.log(pred)).norm(dim=1)
+            loss = torch.mean(abs_diff) # Reducing the loss by a factor
 
         else:
             try:
@@ -467,11 +476,7 @@ class ZLoss(_Loss): # Z - depth
             except:
                 return torch.tensor(float('nan')).cuda().float()   
 
-        # Remove any nans in the data
-        clean_loss = loss[torch.isnan(loss) == False]
-
-        # Return the some of all the losses
-        return torch.mean(clean_loss)
+        return loss
 
 # Scales
 class ScalesLoss(_Loss): # h, w, l scales
@@ -497,7 +502,13 @@ class ScalesLoss(_Loss): # h, w, l scales
             pred = gt_pred_matches[self.key][1]
 
             # Calculating the loss
-            loss = (gt-pred).norm(dim=1)
+            abs_diff = torch.abs(gt-pred)
+
+            # Remove any nans in the data
+            abs_diff = abs_diff[torch.isnan(abs_diff) == False]
+
+            # Calculating the loss
+            loss = torch.mean(abs_diff) # Reducing the loss by a factor
 
         else:
             try:
@@ -505,11 +516,7 @@ class ScalesLoss(_Loss): # h, w, l scales
             except:
                 return torch.tensor(float('nan')).cuda().float()   
 
-        # Remove any nans in the data
-        clean_loss = loss[torch.isnan(loss) == False]
-
-        # Return the some of all the losses
-        return torch.mean(clean_loss)
+        return loss
 
 #-------------------------------------------------------------------------------
 # Pose loss functions

source_code/FastPoseCNN/tools/visualize.py
@@ -140,7 +140,7 @@ def get_visualized_simple_xys(xys):
 def get_visualized_z(z, colormap='viridis'):
 
     # Create norm function to shift data to [0:1]
-    norm = matplotlib.colors.Normalize(vmin=0, vmax=255)
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=1)
 
     # Obtain the colormap of choice
     my_colormap = matplotlib.cm.get_cmap(colormap)
@@ -188,7 +188,7 @@ def get_visualized_quaternion(quaternion, bg='black'):
     empty_quaternion = np.zeros_like(quaternion)
 
     # creating norm function
-    norm = matplotlib.colors.Normalize(vmin=-255, vmax=255)
+    norm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
 
     # Normalize data
     norm_quat = norm(quaternion)
@@ -524,9 +524,9 @@ def compare_all(preds, gts, mask_colormap):
 def compare_mask_performance(image, gt_mask, pred_mask, colormap):
     
     # Converting visual images into np.uint8 for matplotlib compatibility
-    image_vis = skimage.img_as_ubyte(image.cpu().numpy())
-    gt_mask = skimage.img_as_ubyte(gt_mask.cpu().numpy())
-    pred_mask = skimage.img_as_ubyte(pred_mask.cpu().numpy())
+    image_vis = image.cpu().numpy()
+    gt_mask = gt_mask.cpu().numpy()
+    pred_mask = pred_mask.cpu().numpy()
 
     # Colorized the binary masks
     gt_mask_vis = get_visualized_masks(gt_mask, colormap)
@@ -544,10 +544,10 @@ def compare_mask_performance(image, gt_mask, pred_mask, colormap):
 def compare_quat_performance(image, gt_quad, pred_quaternion, pred_mask, mask_colormap):
 
     # Converting visual images into np.uint8 for matplotlib compatibility
-    image_vis = skimage.img_as_ubyte(image.cpu().numpy())
-    gt_quad = skimage.img_as_ubyte(gt_quad.cpu().numpy())
-    pred_quaternion = skimage.img_as_ubyte(pred_quaternion.cpu().numpy())
-    pred_mask = skimage.img_as_ubyte(pred_mask.cpu().numpy())
+    image_vis = image.cpu().numpy()
+    gt_quad = gt_quad.cpu().numpy()
+    pred_quaternion = pred_quaternion.cpu().numpy()
+    pred_mask = pred_mask.cpu().numpy()
 
     # Colorizing the mask
     pred_mask_vis = get_visualized_masks(pred_mask, mask_colormap)
@@ -569,10 +569,10 @@ def compare_quat_performance(image, gt_quad, pred_quaternion, pred_mask, mask_co
 def compare_xy_performance(image, gt_mask, gt_xy, pred_mask, pred_xy, mask_colormap):
 
     # Converting visual images into np.uint8 for matplotlib compatibility
-    image_vis = skimage.img_as_ubyte(image.cpu().numpy())
-    gt_mask = skimage.img_as_ubyte(gt_mask.cpu().numpy())
+    image_vis = image.cpu().numpy()
+    gt_mask = gt_mask.cpu().numpy()
     gt_xy = gt_xy.cpu().numpy()
-    pred_mask = skimage.img_as_ubyte(pred_mask.cpu().numpy())
+    pred_mask = pred_mask.cpu().numpy()
     pred_xy = pred_xy.cpu().numpy()
 
     # Converting visual images into np.uint8 for matplotlib compatibility
@@ -595,10 +595,10 @@ def compare_xy_performance(image, gt_mask, gt_xy, pred_mask, pred_xy, mask_color
 def compare_z_performance(image, gt_z, pred_z, pred_mask, mask_colormap):
 
     # Converting visual images into np.uint8 for matplotlib compatibility
-    image_vis = skimage.img_as_ubyte(image.cpu().numpy())
-    gt_z = skimage.img_as_ubyte((gt_z/torch.max(gt_z)).cpu().numpy())
-    pred_z = skimage.img_as_ubyte((pred_z/torch.max(pred_z)).cpu().numpy())
-    pred_mask = skimage.img_as_ubyte(pred_mask.cpu().numpy())
+    image_vis = image.cpu().numpy()
+    gt_z = (gt_z/torch.max(torch.abs(gt_z))).cpu().numpy()
+    pred_z = (pred_z/torch.max(torch.abs(pred_z))).cpu().numpy()
+    pred_mask = pred_mask.cpu().numpy()
 
     # Converting visual images into np.uint8 for matplotlib compatibility
     pred_mask_vis = get_visualized_masks(pred_mask, mask_colormap)
@@ -620,10 +620,10 @@ def compare_z_performance(image, gt_z, pred_z, pred_mask, mask_colormap):
 def compare_scales_performance(image, gt_scales, pred_scales, pred_mask, mask_colormap):
 
     # Converting visual images into np.uint8 for matplotlib compatibility
-    image_vis = skimage.img_as_ubyte(image.cpu().numpy())
+    image_vis = image.cpu().numpy()
     gt_scales = gt_scales.cpu().numpy()
     pred_scales = pred_scales.cpu().numpy()
-    pred_mask = skimage.img_as_ubyte(pred_mask.cpu().numpy())
+    pred_mask = pred_mask.cpu().numpy()
 
     # Converting visual images into np.uint8 for matplotlib compatibility
     pred_mask_vis = get_visualized_masks(pred_mask, mask_colormap)
@@ -637,7 +637,7 @@ def compare_scales_performance(image, gt_scales, pred_scales, pred_mask, mask_co
         image = image_vis,
         pred_mask = pred_mask_vis,
         gt_scales = gt_scales_vis,
-        pred_z = pred_scales_vis
+        pred_scales = pred_scales_vis
     )
 
     return summary_fig
@@ -682,8 +682,8 @@ def compare_hough_voting_performance(image, preds_agg_data, return_as_figure=Tru
         )
 
     images = {
-        'pred_uv': skimage.img_as_ubyte(drawn_pred_uv.cpu().numpy()),
-        'pred_hv': skimage.img_as_ubyte(drawn_pred_hv.cpu().numpy())
+        'pred_uv': drawn_pred_uv.cpu().numpy(),
+        'pred_hv': drawn_pred_hv.cpu().numpy()
     }
 
     if return_as_figure:

source_code/FastPoseCNN/train.py
@@ -7,8 +7,8 @@ from pprint import pprint
 
 # DEBUGGING
 import pdb
-#import matplotlib
-#matplotlib.use('Agg')
+import matplotlib
+matplotlib.use('Agg')
 
 import numpy as np
 import base64
@@ -95,7 +95,23 @@ class PoseRegresssionTask(pl.LightningModule):
 
     @pl.core.decorators.auto_move_data
     def forward(self, x):
-        return self.model(x)
+
+        # Feed in the input to the actual model
+        y = self.model(x)
+
+        # Ensuring that the first-level outputs (mostly the image-size outputs)
+        # do not have nans for visualization and metric purposes
+        for key in y.keys():
+            if isinstance(y[key], torch.Tensor):
+                # Getting the dangerous conditions
+                is_nan = torch.isnan(y[key])
+                is_inf = torch.isinf(y[key])
+                is_nan_or_inf = torch.logical_or(is_nan, is_inf)
+
+                # Filling with stable information.
+                y[key][is_nan_or_inf] = 0
+
+        return y
 
     def training_step(self, batch, batch_idx):
         
@@ -113,7 +129,7 @@ class PoseRegresssionTask(pl.LightningModule):
             if task_name == 'total_loss' or 'loss' not in multi_task_losses[task_name]:
                 continue
             
-            result.log(f'train_{task_name}_loss', multi_task_losses[task_name]['loss'])
+            result.log(f'train_{task_name}_loss', multi_task_losses[task_name]['task_total_loss'])
 
         # Compress the multi_task_losses and multi_task_metrics to make logging easier
         loggable_metrics = tools.dm.compress_dict(multi_task_metrics)
@@ -152,7 +168,7 @@ class PoseRegresssionTask(pl.LightningModule):
     def shared_step(self, mode, batch, batch_idx):
         
         # Forward pass the input and generate the prediction of the NN
-        outputs = self.model(batch['image'])
+        outputs = self.forward(batch['image'])
 
         # Matching aggregated data between ground truth and predicted
         if self.HPARAM.PERFORM_AGGREGATION and self.HPARAM.PERFORM_MATCHING:
@@ -191,6 +207,9 @@ class PoseRegresssionTask(pl.LightningModule):
                     multi_task_losses['pose']['total_loss'] = losses['task_total_loss']
                 else:
                     multi_task_losses['pose']['total_loss'] += losses['task_total_loss']
+
+        # ! Debugging what is the loss that has large values!
+        #pprint(multi_task_losses)
 
         # Logging the losses
         for task_name in multi_task_losses.keys():
@@ -248,7 +267,7 @@ class PoseRegresssionTask(pl.LightningModule):
             weighted_sum = torch.sum(torch.stack(weighted_losses))
 
             # Save the calculated sum in the losses (for PyTorch progress bar logger)
-            losses['loss'] = weighted_sum
+            #losses['loss'] = weighted_sum
 
             # Saving the total loss (for customized Tensorboard logger)
             losses['task_total_loss'] = weighted_sum #total_loss
@@ -256,7 +275,7 @@ class PoseRegresssionTask(pl.LightningModule):
         else: # otherwise, just pass losses as nan
 
             # Place nan in losses (for PyTorch progress bar logger)
-            losses['loss'] = torch.tensor(float('nan')).to(self.device).float()
+            #losses['loss'] = torch.tensor(float('nan')).to(self.device).float()
 
             # Notate no task-specific total loss (for customized Tensorboard logger)
             losses['task_total_loss'] = torch.tensor(float('nan')).to(self.device).float()
@@ -487,26 +506,26 @@ if __name__ == '__main__':
         'quaternion': {
             #'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='quaternion'), 'weight': 0.2},
             #'loss_pw_qloss': {'D': 'pixel-wise', 'F': lib.loss.PixelWiseQLoss(key='quaternion'), 'weight': 1.0}
-            'loss_quat': {'D': 'matched', 'F': lib.loss.QLoss(key='quaternion'), 'weight': 0.4},
+            'loss_quat': {'D': 'matched', 'F': lib.loss.QLoss(key='quaternion'), 'weight': 1.0},
         },
         'xy': {
             #'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='xy'), 'weight': 0.2},
-            'loss_xy': {'D': 'matched', 'F': lib.loss.XYLoss(key='xy'), 'weight': 0.4},
+            'loss_xy': {'D': 'matched', 'F': lib.loss.XYLoss(key='xy'), 'weight': 1.0},
         },
         'z': {
             #'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='z'), 'weight': 0.2},
-            'loss_z': {'D': 'matched', 'F': lib.loss.ZLoss(key='z'), 'weight': 0.4},
+            'loss_z': {'D': 'matched', 'F': lib.loss.ZLoss(key='z'), 'weight': 1.0},
         },
         'scales': {
             #'loss_mse': {'D': 'pixel-wise', 'F': lib.loss.MaskedMSELoss(key='scales'), 'weight': 0.2},
-            'loss_scales': {'D': 'matched', 'F': lib.loss.ScalesLoss(key='scales'), 'weight': 0.4},
+            'loss_scales': {'D': 'matched', 'F': lib.loss.ScalesLoss(key='scales'), 'weight': 1.0},
         },
-        'RT_and_metrics': {
-            'loss_R': {'D': 'matched', 'F': lib.loss.RLoss(key='R'), 'weight': 1.0},
-            'loss_T': {'D': 'matched', 'F': lib.loss.TLoss(key='T'), 'weight': 1.0},
+        #'RT_and_metrics': {
+        #    'loss_R': {'D': 'matched', 'F': lib.loss.RLoss(key='R'), 'weight': 1.0},
+        #    'loss_T': {'D': 'matched', 'F': lib.loss.TLoss(key='T'), 'weight': 1.0},
         #    'loss_iou3d': {'D': 'matched', 'F': lib.loss.Iou3dLoss(), 'weight': 1.0},
         #    'loss_offset': {'D': 'matched', 'F': lib.loss.OffsetLoss(), 'weight': 1.0}
-        }
+        #}
     }
 
     # Selecting metrics
@@ -675,7 +694,7 @@ if __name__ == '__main__':
         distributed_backend=HPARAM.DISTRIBUTED_BACKEND, # required to work
         logger=tb_logger,
         callbacks=[custom_callback, loss_checkpoint_callback],
-        gradient_clip_val=0.5
+        gradient_clip_val=0.25
     )
 
     # Train