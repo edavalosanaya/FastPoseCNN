Fixed missing None type catch of pred_gt_matches variable and better balance the losses between xy, scales, z, mask, and quaternion. Changed the epoch level data's x axis to be the epoch number rather than the sample step number. Additionally, implemented better model ckpts (every 5 epochs).

source_code/FastPoseCNN/evaluate.py
@@ -32,8 +32,8 @@ from config import DEFAULT_POSE_HPARAM
 #-------------------------------------------------------------------------------
 # Constants
 
-PATH = pathlib.Path(os.getenv("LOGS")) / 'good_saved_runs' / '21-38-XY_REGRESSION_DEBUG-CAMERA-resnet18-imagenet' / '_' / 'checkpoints' / 'last.ckpt'
-#PATH = pathlib.Path(os.getenv("LOGS")) / 'good_saved_runs' / '23-16-CAM_LAPTOP_BASELINE-CAMERA-resnet18-imagenet' / '_' / 'checkpoints' / 'last.ckpt'
+#PATH = pathlib.Path(os.getenv("LOGS")) / 'good_saved_runs' / '2_object' / '21-38-XY_REGRESSION_DEBUG-CAMERA-resnet18-imagenet' / '_' / 'checkpoints' / 'last.ckpt'
+PATH = pathlib.Path(os.getenv("LOGS")) / 'good_saved_runs' / 'all_object' / '23-52-ALL_OBJECTS_E25-CAMERA-resnet18-imagenet' / '_' / 'checkpoints' / 'last.ckpt'
 
 HPARAM = DEFAULT_POSE_HPARAM()
 HPARAM.VALID_SIZE = 2000
@@ -212,8 +212,6 @@ if __name__ == '__main__':
         # Load the .pth file with the tensors
         all_matches = torch.load(pth_path)
 
-        pdb.set_trace()
-
         if all_matches == []:
             print("All matches made were empty!")
             sys.exit(0)
@@ -266,8 +264,6 @@ if __name__ == '__main__':
                     raw_data['degree_error'][int(class_id)].append(degree_distance)
                     raw_data['3d_iou'][int(class_id)].append(ious_3d)
                     raw_data['offset_error'][int(class_id)].append(offset_errors)
-
-        pdb.set_trace()
 
         # After the loop of the matches
         for class_id in range(1, len(HPARAM.SELECTED_CLASSES)): # -1 to remove bg

source_code/FastPoseCNN/lib/loss.py
@@ -259,7 +259,10 @@ class QLoss(_Loss): # Quaternion
             try:
                 return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()
 
         # Remove any nans in the data
         clean_loss = loss[torch.isnan(loss) == False]
@@ -306,11 +309,11 @@ class QLoss(_Loss): # Quaternion
         # Performing dot product on the transformed e_pred and e_gt
         dot_product = torch.einsum('bij,bij->bi', rot_e_pred.double(), e_gt.double())
 
-        # Determine the maximum along the first dimension (rotated dimension)
-        best_alignment = torch.max(dot_product, dim=1).values
-
         # Calculating the loss
-        loss = self.dot_product_to_loss(best_alignment)
+        all_loss = self.dot_product_to_loss(dot_product)
+
+        # Obtain the smallest loss and return that
+        loss = torch.min(all_loss, dim=1).values
 
         return loss
 
@@ -353,7 +356,10 @@ class RLoss(_Loss): # Rotation Matrix
             try:
                 return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()
 
         # Remove any nans in the data
         clean_loss = loss[torch.isnan(loss) == False]
@@ -391,7 +397,10 @@ class TLoss(_Loss): # Translation Vector
             try:
                 return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()  
 
         # Remove any nans in the data
         clean_loss = loss[torch.isnan(loss) == False]
@@ -428,13 +437,16 @@ class XYLoss(_Loss): # 2D Center
             abs_diff = abs_diff[torch.isnan(abs_diff) == False]
 
             # Calculating the loss
-            loss = torch.mean(abs_diff) / 10 # Reducing the loss by a factor 
+            loss = torch.mean(abs_diff)
 
         else:
             try:
                 return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float() 
 
         # Return the some of all the losses
         return loss
@@ -474,7 +486,10 @@ class ZLoss(_Loss): # Z - depth
             try:
                 return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()  
 
         return loss
 
@@ -514,7 +529,10 @@ class ScalesLoss(_Loss): # h, w, l scales
             try:
                 return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()  
 
         return loss
 
@@ -550,9 +568,12 @@ class Iou3dLoss(_Loss):
 
         else:
             try:
-                return torch.tensor(float('nan'), device=gt_pred_matches[0]['instance_masks'].device).float()   
+                return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()
 
         # Remove any nans in the data
         clean_loss = loss[torch.isnan(loss) == False]
@@ -587,9 +608,12 @@ class OffsetLoss(_Loss):
 
         else:
             try:
-                return torch.tensor(float('nan'), device=gt_pred_matches[0]['instance_masks'].device).float()   
+                return torch.tensor(float('nan'), device=gt_pred_matches['instance_masks'].device).float()   
             except:
-                return torch.tensor(float('nan')).cuda().float()   
+                try:
+                    return torch.tensor(float('nan')).cuda().float()
+                except:
+                    return torch.tensor(float('nan')).float()  
 
         # Remove any nans in the data
         clean_loss = loss[torch.isnan(loss) == False]

source_code/FastPoseCNN/logger.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 from pathlib import Path
+from matplotlib import use
 
 from torch.utils.tensorboard.writer import SummaryWriter
 
@@ -113,11 +114,14 @@ class MyLogger(pl.loggers.LightningLoggerBase):
         pass
 
     @rank_zero_only
-    def log_metrics(self, mode, metrics, batch_idx, store=True):
+    def log_metrics(self, mode, metrics, batch_idx, store=True, use_epoch_num=True):
 
         # Calculate global step, since given information regarding the step is 
         # given in batch_idx
-        step = self.calculate_global_step(mode, batch_idx)
+        if use_epoch_num:
+            step = batch_idx
+        else:
+            step = self.calculate_global_step(mode, batch_idx)
 
         # Logging the metrics invidually to tensorboard
         for metric_name, metric_value in metrics.items():