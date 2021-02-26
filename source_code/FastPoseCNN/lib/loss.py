Fixed disappearing instances and Nan gradients/weights due to invalid and corrupt samples.

source_code/FastPoseCNN/lib/loss.py
@@ -431,7 +431,7 @@ class XYLoss(_Loss): # 2D Center
             pred = gt_pred_matches[self.key][1]
 
             # Calculating the loss
-            abs_diff = torch.abs(gt-pred)
+            abs_diff = (gt-pred).norm(dim=1)
 
             # Remove any nans in the data
             abs_diff = abs_diff[torch.isnan(abs_diff) == False]
@@ -474,13 +474,13 @@ class ZLoss(_Loss): # Z - depth
             pred = gt_pred_matches[self.key][1]
 
             # Calculating the loss (logging the data first)
-            abs_diff = torch.abs(torch.log(gt)-torch.log(pred))
+            abs_diff = (torch.log(gt)-torch.log(pred)).norm(dim=1)
 
             # Remove any nans in the data
-            abs_diff = abs_diff[torch.isnan(abs_diff) == False]
+            clean_diff = abs_diff[torch.isnan(abs_diff) == False]
 
             # Calculating the loss
-            loss = torch.mean(abs_diff) # Reducing the loss by a factor
+            loss = torch.mean(clean_diff) # Reducing the loss by a factor
 
         else:
             try:
@@ -517,7 +517,7 @@ class ScalesLoss(_Loss): # h, w, l scales
             pred = gt_pred_matches[self.key][1]
 
             # Calculating the loss
-            abs_diff = torch.abs(gt-pred)
+            abs_diff = (gt-pred).norm(dim=1)
 
             # Remove any nans in the data
             abs_diff = abs_diff[torch.isnan(abs_diff) == False]

source_code/FastPoseCNN/tools/dataset.py
@@ -5,8 +5,10 @@ import pathlib
 import collections
 from typing import List, Union
 import abc
+import pprint
 
 import pdb
+import logging
 
 import random
 import numpy as np
@@ -47,6 +49,7 @@ import transforms
 
 ENCODER = 'resnext50_32x4d'
 ENCODER_WEIGHTS = 'imagenet'
+LOGGER = logging.getLogger('fastposecnn')
 
 #-------------------------------------------------------------------------------
 # Custom Abstract Dataset
@@ -120,9 +123,12 @@ class NOCSDataset(Dataset, torch.utils.data.Dataset):
     def __getitem__(self, i):
 
         # DEBUGGING
-        # self.images_fps[i] = pathlib.Path(
-        #     '/home/students/edavalos/GitHub/FastPoseCNN/datasets/NOCS/camera/val/01916/0009_color.png'
-        # )
+        """
+        self.images_fps[i] = pathlib.Path(
+            '/home/students/edavalos/GitHub/FastPoseCNN/datasets/NOCS/camera/val/01173/0007_color.png'
+        )
+        """
+        LOGGER.debug(f"LOADING IMAGE: {str(self.images_fps[i])}")
 
         # Reading data
         # Image
@@ -185,10 +191,15 @@ class NOCSDataset(Dataset, torch.utils.data.Dataset):
         # Generate the aggregated data (instance-lead data)
         agg_data = self.generate_agg_data(good_instances_mask, good_json_data)
 
+        # Check if any data is invalid, if it is, simply return None for the sample
+        if (agg_data['z'] <= 0).any():
+            LOGGER.debug(f"INVALID/CORRUPT SAMPLE: z <= 0, class_ids: {agg_data['class_ids']}")
+            return None
+
         # Create dense representation of the data (class type is instances)
-        quaternions = dm.create_dense_quaternion(good_instances_mask, good_json_data)
-        scales = dm.create_dense_scales(good_instances_mask, good_json_data)
-        xy, z = dm.create_dense_3d_centers(good_instances_mask, good_json_data, self.INTRINSICS)
+        #quaternions = dm.create_dense_quaternion(good_instances_mask, good_json_data)
+        #scales = dm.create_dense_scales(good_instances_mask, good_json_data)
+        #xy, z = dm.create_dense_3d_centers(good_instances_mask, good_json_data, self.INTRINSICS)
 
         # Generating class mask with the desired object class
         class_mask = np.zeros_like(good_instances_mask)
@@ -200,10 +211,10 @@ class NOCSDataset(Dataset, torch.utils.data.Dataset):
             'clean_image': image,
             'image': image, 
             'mask': class_mask, 
-            'quaternion': quaternions,
-            'scales': scales,
-            'xy': xy,
-            'z': z
+            #'quaternion': quaternions,
+            #'scales': scales,
+            #'xy': xy,
+            #'z': z
         }
 
         # apply augmentations
@@ -227,10 +238,10 @@ class NOCSDataset(Dataset, torch.utils.data.Dataset):
         sample.update({
             'image': skimage.img_as_float32(sample['image']),
             'mask': sample['mask'].astype('long'),
-            'quaternion': skimage.img_as_float32(sample['quaternion']),
-            'scales': skimage.img_as_float32(sample['scales']),
-            'xy': skimage.img_as_float32(sample['xy']),
-            'z': skimage.img_as_float32(sample['z']),
+            #'quaternion': skimage.img_as_float32(sample['quaternion']),
+            #'scales': skimage.img_as_float32(sample['scales']),
+            #'xy': skimage.img_as_float32(sample['xy']),
+            #'z': skimage.img_as_float32(sample['z']),
             'agg_data': agg_data
         })
 
@@ -416,6 +427,9 @@ class REALDataset(NOCSDataset):
 # Dataloader
 
 def my_collate_fn(batch, device=None):
+
+    # Filtering any samples that were deemed corrupted or invalid
+    batch = list(filter(lambda x : x is not None, batch))
 
     collate_batch = {}
     agg_data = {

source_code/FastPoseCNN/tools/visualize.py
@@ -1048,18 +1048,18 @@ def compare_all_performance(sample, outputs, pred_gt_matches, intrinsics, mask_c
     gt_mask_vis = get_visualized_masks(sample['mask'], mask_colormap)
 
     # z
-    gt_z_vis = get_visualized_zs(sample['z'])
+    #gt_z_vis = get_visualized_zs(sample['z'])
     pred_z_vis = get_visualized_zs(outputs['z'])
 
     # Ground truth xy
-    gt_uv = get_visualized_u_vector_xys(sample['mask'], sample['xy'].cpu().numpy())
+    #gt_uv = get_visualized_u_vector_xys(sample['mask'], sample['xy'].cpu().numpy())
 
     # scales
-    gt_scales_vis = get_visualized_scales(sample['scales'].cpu().numpy())
+    #gt_scales_vis = get_visualized_scales(sample['scales'].cpu().numpy())
     pred_scales_vis = get_visualized_scales(outputs['scales'].cpu().numpy())
 
     # quaternion
-    gt_quat_vis = get_visualized_quaternions(sample['quaternion'].cpu().numpy())
+    #gt_quat_vis = get_visualized_quaternions(sample['quaternion'].cpu().numpy())
     pred_quat_vis = get_visualized_quaternions(outputs['quaternion'].cpu().numpy())
 
     # If only matches are found
@@ -1083,10 +1083,10 @@ def compare_all_performance(sample, outputs, pred_gt_matches, intrinsics, mask_c
 
         gt_images = {
             'gt_mask': np.moveaxis(gt_mask_vis, 1,-1),
-            'gt_z': gt_z_vis,
-            'gt_scales': gt_scales_vis,
-            'gt_quat': gt_quat_vis,
-            'gt_uv': gt_uv
+            #'gt_z': gt_z_vis,
+            #'gt_scales': gt_scales_vis,
+            #'gt_quat': gt_quat_vis,
+            #'gt_uv': gt_uv
         }
 
         pred_images = {
@@ -1106,9 +1106,9 @@ def compare_all_performance(sample, outputs, pred_gt_matches, intrinsics, mask_c
 
         gt_images = {
             'gt_mask': np.moveaxis(gt_mask_vis, 1,-1),
-            'gt_z': gt_z_vis,
-            'gt_scales': gt_scales_vis,
-            'gt_quat': gt_quat_vis
+            #'gt_z': gt_z_vis,
+            #'gt_scales': gt_scales_vis,
+            #'gt_quat': gt_quat_vis
         }
 
         pred_images = {

source_code/FastPoseCNN/train.py
@@ -189,6 +189,9 @@ class PoseRegresssionTask(pl.LightningModule):
             )
         else:
             gt_pred_matches = None
+
+        LOGGER.debug("\nMATCHED DATA")
+        LOGGER.debug(pprint.pformat(gt_pred_matches))
         
         # Storage for losses and metrics depending on the task
         multi_task_losses = {'pose': {'total_loss': torch.tensor(0).float().to(self.device)}}
@@ -219,7 +222,7 @@ class PoseRegresssionTask(pl.LightningModule):
                     multi_task_losses['pose']['total_loss'] += losses['task_total_loss']
 
         # ! Debugging what is the loss that has large values!
-        LOGGER.debug(pprint.pformat(batch['agg_data']))
+        LOGGER.debug("\nALL LOSSESS")
         LOGGER.debug(pprint.pformat(multi_task_losses))
 
         # Logging the losses
@@ -335,6 +338,18 @@ class PoseRegresssionTask(pl.LightningModule):
                     metrics[metric_name] = metric_attrs['F'](gt_pred_matches)
 
         return metrics
+
+    def on_after_backward(self) -> None:
+        # ! Debugging purposes
+        #"""
+        for section_name, params in {'translation_decoder': self.model.translation_decoder, 'translation_head': self.model.translation_head}.items():
+            LOGGER.debug(f"\nSEEING {section_name} parameters")
+            for name, param in params.named_parameters():
+                if type(param.grad) != type(None):
+                    LOGGER.debug(f'{name}: max={torch.max(torch.abs(param.grad))} nan={torch.isfinite(param.grad).all()}')
+                else:
+                    LOGGER.debug(f'{name}: {param.grad}')
+        #"""
 
     def configure_optimizers(self):
 
@@ -563,6 +578,7 @@ if __name__ == '__main__':
             'iou': {'D': 'pixel-wise', 'F': pl.metrics.functional.iou},
             'f1': {'D': 'pixel-wise', 'F': pl.metrics.functional.f1_score}
         },
+        """
         'quaternion': {
             'mae': {'D': 'pixel-wise', 'F': pl.metrics.functional.mean_absolute_error},
         },
@@ -575,6 +591,7 @@ if __name__ == '__main__':
         'scales': {
             'mae': {'D': 'pixel-wise', 'F': pl.metrics.functional.mean_absolute_error}
         },
+        """
         'pose': {
             'degree_error': {'D': 'matched', 'F': lib.metrics.DegreeError()},
             'degree_error_AP_5': {'D': 'matched', 'F': lib.metrics.DegreeErrorMeanAP(5)},
@@ -685,6 +702,12 @@ if __name__ == '__main__':
         name=run_name
     )
 
+    # Add logging for debugging long sessions
+    logging.basicConfig(
+        filename=str(run_of_the_day_dir / run_name  / 'run.log'),
+        level=logging.DEBUG
+    )
+
     # A callback for creating the visualization and logging data to Tensorboard
     tensorboard_callback = plc.TensorboardCallback(
         HPARAM=HPARAM,
@@ -699,7 +722,6 @@ if __name__ == '__main__':
     ckpt_save_n_callback = plc.CheckpointEveryNSteps(
         save_step_frequency = HPARAM.CKPT_SAVE_FREQUENCY,
         prefix = 'n-ckpt'
-
     )
 
     # Checkpoint callbacks
@@ -721,12 +743,6 @@ if __name__ == '__main__':
     )
     """
 
-    # Add logging for debugging long sessions
-    logging.basicConfig(
-        filename=str(run_of_the_day_dir / run_name  / 'run.log'),
-        level=logging.DEBUG
-    )
-
     # Training
     trainer = pl.Trainer(
         max_epochs=HPARAM.NUM_EPOCHS,
@@ -738,7 +754,7 @@ if __name__ == '__main__':
             tensorboard_callback, 
             loss_checkpoint_callback,
             ckpt_save_n_callback],
-        gradient_clip_val=0.25
+        gradient_clip_val=0.15
     )
 
     # Train