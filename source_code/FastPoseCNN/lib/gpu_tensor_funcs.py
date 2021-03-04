Fixed symmetric handling of quaternions. YAYAYAYAYAY!

source_code/FastPoseCNN/lib/gpu_tensor_funcs.py
@@ -884,7 +884,8 @@ def quat_symmetric_tf(tf_q, ex_q):
     if not hasattr(quat_symmetric_tf, 'rot_q'):
         
         # Creating all the degrees of rotation
-        #degrees = torch.tensor([0,45,90], device=tf_q.device).float() #
+        #degrees = torch.tensor([0,90,180,270], device=tf_q.device).float() #
+        #degrees = torch.arange(0, 360, 15).float()
         degrees = torch.arange(0, 360).float()
         factor = torch.sin(torch.deg2rad(degrees) / 2)
         
@@ -892,7 +893,11 @@ def quat_symmetric_tf(tf_q, ex_q):
         w = torch.cos(torch.deg2rad(degrees) / 2)
         ones = (1 * factor)
         zeros = (0 * factor)
-        x, y, z = zeros, zeros, ones
+        
+        #x, y, z = ones, zeros, zeros
+        x, y, z = zeros, ones, zeros # This is the correct one!
+        #x, y, z = zeros, zeros, ones
+
         
         # Constructing the overall rotation quaternions
         quat_symmetric_tf.rot_q = torch.vstack((w, x, y, z)).T

source_code/FastPoseCNN/lib/loss.py
@@ -1,8 +1,22 @@
+import sys
+import os
+
 import torch
 from torch import Tensor, nn
 from torch.nn.modules.loss import _Loss
 from pytorch_toolbelt.losses.focal import FocalLoss
 import gpu_tensor_funcs as gtf
+
+# For debugging plotting!
+import matplotlib.pyplot as plt
+
+# Local imports
+sys.path.append(os.getenv("TOOLS_DIR"))
+
+try:
+    import visualize as vz
+except ImportError:
+    pass
 
 #-------------------------------------------------------------------------------
 # Mask Losses
@@ -305,10 +319,12 @@ class QLoss(_Loss): # Quaternion
         # Expanding the pred to account for 0-360 degrees of rotation to account 
         # for z-axis symmetric and expanding the gt to match its size for later 
         # comparison
-        rot_e_pred, e_gt = gtf.quat_symmetric_tf(pred, gt)
+        #rot_e_pred, e_gt = gtf.quat_symmetric_tf(pred, gt)
+        rot_e_gt, e_pred = gtf.quat_symmetric_tf(gt, pred)
 
         # Performing dot product on the transformed e_pred and e_gt
-        dot_product = torch.einsum('bij,bij->bi', rot_e_pred.double(), e_gt.double())
+        #dot_product = torch.einsum('bij,bij->bi', rot_e_pred.double(), e_gt.double())
+        dot_product = torch.einsum('bij,bij->bi', e_pred.double(), rot_e_gt.double())
 
         # Calculating the loss
         all_loss = self.dot_product_to_loss(dot_product)
@@ -621,10 +637,3 @@ class OffsetLoss(_Loss):
 
         # Return the some of all the losses
         return torch.mean(clean_loss)
-
-
-
-
-
-
-

source_code/FastPoseCNN/tools/data_manipulation.py
@@ -1385,10 +1385,10 @@ def q_mult(q1, q2):
 
 def q_conjugate(q):
     q_c = q.copy()
-    q_c[...,1:] = -1 * q_c[...,1:]
+    q_c[..., 1:] = -1 * q_c[..., 1:]
     return q_c
 
 def qv_mult(q1, v):
     q2 = np.concatenate((np.array([0]), v))
-    result = q_mult(q_mult(q1, q2), q_conjugate(q1))[:, 1:]
+    result = q_mult(q_mult(q1, q2), q_conjugate(q1))[..., 1:]
     return normalize(result, axis=-1)

source_code/FastPoseCNN/tools/visualize.py
@@ -1264,9 +1264,12 @@ def plot_aps(
 #-------------------------------------------------------------------------------
 # Plot quaternions
 
-def plot_quaternions(qs):
+def plot_quaternions(qs, name=None):
     
-    origin = np.zeros((qs.shape[0], 3))
+    if len(qs.shape) >= 2:
+        origin = np.zeros((qs.shape[0], 3))
+    else:
+        origin = np.zeros((3,))
 
     x_axis = np.array([1,0,0])
     y_axis = np.array([0,1,0])
@@ -1276,27 +1279,60 @@ def plot_quaternions(qs):
     r_y_axis = dm.qv_mult(qs, y_axis)
     r_z_axis = dm.qv_mult(qs, z_axis)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim3d(-1.25,1.25)
-    ax.set_ylim3d(-1.25,1.25)
-    ax.set_zlim3d(-1.25,1.25)
+    # More on how to change the viewpoint of the 3D plot
+    # https://stackoverflow.com/questions/12904912/how-to-set-camera-position-for-3d-plots-using-python-matplotlib
 
-    for v, c in zip([x_axis, y_axis, z_axis], ['red', 'blue', 'green']):
-        ax.quiver(
-            origin[:,0], origin[:,1], origin[:,2],
-            v[0], v[1], v[2],
-            color = c, alpha = 1, arrow_length_ratio = 0.1, normalize=True,
-            length = 1
-        )
+    fig = plt.figure(figsize=plt.figaspect(0.25))
+
+    if name:
+        fig.suptitle(name)
+
+    ax0 = fig.add_subplot(141, projection='3d') # Isometric
+    ax1 = fig.add_subplot(142, projection='3d') # top-view
+    ax2 = fig.add_subplot(143, projection='3d') # front-view
+    ax3 = fig.add_subplot(144, projection='3d') # side-view
     
-    for v, c in zip([r_x_axis, r_y_axis, r_z_axis], ['red', 'blue', 'green']):
-        ax.quiver(
-            origin[:,0], origin[:,1], origin[:,2],
-            v[:,0], v[:,1], v[:,2],
-            color = c, alpha = 0.25, arrow_length_ratio = 0.1, normalize=True,
-            length = 1
-        )
+    for ax in [ax0, ax1, ax2, ax3]:
+
+        ax.set_xlim3d(-1.25,1.25)
+        ax.set_ylim3d(-1.25,1.25)
+        ax.set_zlim3d(-1.25,1.25)
+
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+
+        for v, c in zip([x_axis, y_axis, z_axis], ['red', 'blue', 'green']):
+            ax.quiver(
+                origin[...,0], origin[...,1], origin[...,2],
+                v[0], v[1], v[2],
+                color = c, alpha = 1, arrow_length_ratio = 0.1, normalize=True,
+                length = 1.1
+            )
+        
+        for v, c in zip([r_x_axis, r_y_axis, r_z_axis], ['red', 'blue', 'green']):
+            ax.quiver(
+                origin[...,0], origin[...,1], origin[...,2],
+                v[...,0], v[...,1], v[...,2],
+                color = c, alpha = 0.25, arrow_length_ratio = 0.1, normalize=True,
+                length = 1.1
+            )
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+
+    # Now setting the viewpoint of each plot
+    #ax0.azim, ax0.dist, ax0.elev = (-60, 10, 30) # Isometric
+    
+    ax0.title.set_text('Isometric')
+    
+    ax1.title.set_text('Top-View')
+    ax1.view_init(azim=0, elev=90) # xy-plane
+    
+    ax2.title.set_text('Front-View')
+    ax2.view_init(azim=0, elev=0) # yz-plane
+    
+    ax3.title.set_text("Side-View")
+    ax3.view_init(azim=-90, elev=0) # zx-plane
 
     return fig
 
@@ -1306,7 +1342,7 @@ def plot_quaternions(qs):
 if __name__ == '__main__':
 
     # Initialize simple quaternions
-    q = np.array([[0.909,0.001,0.395,-0.132]])
+    q = np.array([0.909,0.001,0.395,-0.132])
 
     fig = plot_quaternions(q)
     plt.show()